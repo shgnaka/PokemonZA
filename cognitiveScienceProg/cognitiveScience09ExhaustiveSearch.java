f5b80363fb531caa3b9196c184254438cd2fa621
shgnaka <shogonakamurawppt@gmail.com>
2024-12-09 10:46:09 +0900

    feat: add cognitiveScience09exhaustiveSearch.java and change file name, define nodes

---
+++ cognitiveScienceProg/cognitiveScience09ExhaustiveSearch.java
@@ -0,0 +1,15 @@
+public class cognitiveScience09ExhaustiveSearch {
+
+    public static void main(String[] args) {
+        int contactWithFive, 
+            contactWithFourFst, 
+            contactWithFourSec, 
+            contactWithFourThrd,
+            contactWithThreeFst,
+            contactWithThreeSec,
+            contactWithThreeThrd,
+            contactWithThreeFrth,
+            contactWithThreeFif,
+            contactWithTwo;
+    }
+}
