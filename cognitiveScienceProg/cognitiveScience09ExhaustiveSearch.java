53d5645067253e3a8e7c051536df3e7b9d1126d0
shgnaka <shogonakamurawppt@gmail.com>
2024-12-09 11:32:37 +0900

    feat: delete previous defined variables and, declare and initialize node array

--- cognitiveScienceProg/cognitiveScience09ExhaustiveSearch.java
+++ cognitiveScienceProg/cognitiveScience09ExhaustiveSearch.java
@@ -1,16 +1,16 @@
 public class cognitiveScience09ExhaustiveSearch {
 
     public static void main(String[] args) {
-        long nodeContactWithFive, 
-            nodeContactWithFourFst, 
-            nodeContactWithFourSec, 
-            nodeContactWithFourThrd,
-            nodeContactWithThreeFst,
-            nodeContactWithThreeSec,
-            nodeContactWithThreeThrd,
-            nodeContactWithThreeFrth,
-            nodeContactWithThreeFif,
-            nodeContactWithTwo;
-        long subtractionNodeContactWithFiveFromnodeContactWithFourFst = (nodeContactWithFive - nodeContactWithFourFst) * (nodeContactWithFive - nodeContactWithFourFst);
+        String[] node = new String[10];
+        node[0] = "node1";
+        node[1] = "node2";
+        node[2] = "node3";
+        node[3] = "node4";
+        node[4] = "node5";
+        node[5] = "node6";
+        node[6] = "node7";
+        node[7] = "node8";
+        node[8] = "node9";
+        node[9] = "node10";
     }
 }
