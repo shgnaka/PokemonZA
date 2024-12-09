78be046888dda048782834f75e72015943cfe1e3
shgnaka <shogonakamurawppt@gmail.com>
2024-12-09 13:38:09 +0900

    feat: change node array type String to int

--- cognitiveScienceProg/cognitiveScience09ExhaustiveSearch.java
+++ cognitiveScienceProg/cognitiveScience09ExhaustiveSearch.java
@@ -1,16 +1,7 @@
 public class cognitiveScience09ExhaustiveSearch {
 
     public static void main(String[] args) {
-        String[] node = new String[10];
-        node[0] = "node1";
-        node[1] = "node2";
-        node[2] = "node3";
-        node[3] = "node4";
-        node[4] = "node5";
-        node[5] = "node6";
-        node[6] = "node7";
-        node[7] = "node8";
-        node[8] = "node9";
-        node[9] = "node10";
+        int[] node = new int[10];
+
     }
 }
